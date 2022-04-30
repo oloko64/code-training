0af36165d178096d419b2fd80ed85b6075766a85
OLoKo64 <naldoepingo1@gmail.com>
2022-04-30 17:46:24 -0300

    feat: add crawler script

---
+++ python/crawler.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup as bs4
+from time import time
+
+start_link = 'https://www.google.com/'
+url_filter = ''
+levels_deep = 1
+
+
+def timer(func):
+    def inner(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        print()
+        print(f'Time taken: {round(end - start, 2)} seconds')
+        return result
+    return inner
+
+
+def get_links(link):
+    req = requests.get(link)
+    soup = bs4(req.content, 'html.parser')
+
+    links = [link.get('href') for link in soup.findAll('a')]
+    return [
+        link for link in links if link is not None and link.startswith('http')]
+
+
+def print_link(url_filter, link):
+    if url_filter and url_filter in link:
+        print(link)
+    else:
+        print(link)
+
+
+def crawler_loop(n, loop_link):
+    if n == 0:
+        return
+    else:
+        for link in get_links(loop_link):
+            print_link(url_filter, link)
+            crawler_loop(n - 1, link)
+
+
+@timer
+def crawler():
+    crawler_loop(levels_deep, start_link)
+
+
+crawler()
