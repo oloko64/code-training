e844af750f3d9c5427b47daad3d141c00c8357b2
OLoKo64 <naldoepingo1@gmail.com>
2022-07-30 19:08:47 -0300

    feat: add promises vs async await

---
+++ javascript/promises.ts
@@ -0,0 +1,38 @@
+// Promises are a way to handle asynchronous code.
+function promises(): Promise<string | number> {
+  return new Promise((resolve, reject) => {
+    let n1 = 6;
+    let n2 = 4;
+    if (n1 > n2) {
+      setTimeout(() => {
+        resolve(n1 + n2);
+      }, 1000);
+    } else {
+      reject("Fails");
+    }
+  });
+}
+
+// Async/Await
+async function asyncAwait(): Promise<void> {
+  try {
+    let result = await promises();
+    console.log("Async/Await ----> " + result);
+  } catch (error) {
+    console.log("Async/Await ----> " + error);
+  }
+}
+
+console.log("Start Promise");
+promises()
+  .then((result) => {
+    console.log("Promise -----> " + result);
+  })
+  .catch((error) => {
+    console.log("Promise -----> " + error);
+  });
+console.log("End Promise");
+
+console.log("Start Async/Await");
+asyncAwait();
+console.log("End Async/Await");
