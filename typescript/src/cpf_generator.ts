9a517b92f594cd80c17abc9bd57a48dd59a998f4
OLoKo64 <naldoepingo1@gmail.com>
2022-08-02 20:35:59 -0300

    feat: add some changes

--- typescript/src/cpf_generator.ts
+++ typescript/src/cpf_generator.ts
@@ -1,7 +1,25 @@
 interface CpfTypes {
-    cleanCpf: string;
-    formattedCpf: string;
-  }
+  cleanCpf: string;
+  formattedCpf: string;
+}
+
+const calculate = (): CpfTypes => {
+  const baseValue = randomCpfSeed()
+  let vecSum = baseValue.split('').map((x) => parseInt(x))
+  let vecMultiplied = vecSum.map((_, index) => vecSum[index] * (10 - Number(index)))
+
+  const verifierOne = calcVerifierNum(vecMultiplied.reduce((a, b) => a + b))
+  const baseSecondRun = (baseValue + verifierOne).slice(1)
+
+  vecSum = baseSecondRun.split('').map((x) => parseInt(x))
+  vecMultiplied = vecSum.map((_, index) => vecSum[index] * (10 - Number(index)))
+
+  const verifierTwo = calcVerifierNum(vecMultiplied.reduce((a, b) => a + b))
+  const cleanCPF = `${baseValue}${verifierOne}${verifierTwo}`
+  const formattedCPF = cleanCPF.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+
+  return { cleanCpf: cleanCPF, formattedCpf: formattedCPF }
+}
 
 const randomCpfSeed = () => {
   let cpf = ''
@@ -15,29 +33,12 @@ const calcVerifierNum = (n1: number) => {
   const n2 = n1 % 11
   if (n2 < 2) {
     return 0
+  } else {
+    return 11 - n2
   }
-  return 11 - n2
 }
 
-const calculate = (): CpfTypes => {
-  const baseValue = randomCpfSeed()
-  let vecSum = baseValue.split('').map((x) => parseInt(x, 10))
-  let vecMultiplied = vecSum.map((_, index) => vecSum[index] * (10 - Number(index)))
+const CPFs = calculate()
 
-  const verifierOne = calcVerifierNum(vecMultiplied.reduce((a, b) => a + b))
-  const baseSecondRun = (baseValue + verifierOne).slice(1)
-
-  vecSum = baseSecondRun.split('').map((x) => parseInt(x, 10))
-  vecMultiplied = vecSum.map((_, index) => vecSum[index] * (10 - Number(index)))
-
-  const verifierTwo = calcVerifierNum(vecMultiplied.reduce((a, b) => a + b))
-
-  const cleanCPF = `${baseValue}${verifierOne}${verifierTwo}`
-  const formattedCPF = cleanCPF.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
-
-  console.log(cleanCPF)
-  console.log(formattedCPF)
-  return { cleanCpf: cleanCPF, formattedCpf: formattedCPF }
-}
-
-calculate()
+console.log(CPFs.cleanCpf)
+console.log(CPFs.formattedCpf)
